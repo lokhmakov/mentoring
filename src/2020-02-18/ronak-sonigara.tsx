4ccd62fb305d8447752ed226390b2c2b9e4fcf8e
Pavel Lokhmakov <l.pavel.m@gmail.com>
2020-02-18 18:10:19 +0300

    Ronak Sonigara

--- src/2020-02-18/ronak-sonigara.tsx
+++ src/2020-02-18/ronak-sonigara.tsx
@@ -1,19 +1,38 @@
 import {render} from 'react-dom'
 import * as React from 'react'
 
-function App() {
-  const [state, setState] = React.useState([`first`, `second`])
+const wait = s => new Promise(rs => setTimeout(rs, s * 1000))
 
-  console.log(`state`, state)
+async function query() {
+  await wait(3)
+  return [
+    {id: 1, name: `name1`},
+    {id: 2, name: `name2`},
+    {id: 3, name: `name3`},
+  ]
+}
+
+function App() {
+  const [state, setState] = React.useState([{id: `0`, name: `name0`}])
+  const [isLoading, setLoading] = React.useState(false)
 
   React.useEffect(() => {
-    setState([`Update me`])
+    async function fetch() {
+      setLoading(true)
+      const list = await query()
+      setState(list)
+      setLoading(false)
+    }
+
+    fetch()
   }, [])
+
+  if (isLoading) return `isLoading`
 
   return (
     <div>
-      {state.map((name, key) => (
-        <div key={key}>{name}</div>
+      {state.map(({id, name}) => (
+        <div key={id}>{name}</div>
       ))}
     </div>
   )

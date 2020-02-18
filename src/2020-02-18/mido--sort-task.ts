1f4a5e0aa439e8907f5ddb15dc26228b1e4fcee3
Pavel Lokhmakov <l.pavel.m@gmail.com>
2020-02-18 18:38:03 +0300

    Stepper #3092

--- src/2020-02-18/mido--sort-task.ts
+++ src/2020-02-18/mido--sort-task.ts
@@ -1,9 +1,20 @@
 const SORT_ORDER = [`one`, `two`, `three`, `four`, `five`, `six`]
 const arr = ['three', 'four', 'two', 'one', 'five', 'six']
 
-// START OF SORT BLOCK YOU ARE ALLOWED TO MODIFY
-const map = SORT_ORDER.reduce((r, v, i) => ((r[v] = i), r), {})
-const sortedArr = arr.sort((a, b) => map[a] - map[b])
-// END OF SORT BLOCK YOU ARE ALLOWED TO MODIFY
+function case1() {
+  const map = SORT_ORDER.reduce((r, v, i) => ((r[v] = i), r), {})
+  const sortedArr = arr.sort((a, b) => map[a] - map[b])
 
-console.log('sortedArr', sortedArr)
+  console.log('sortedArr', sortedArr)
+}
+
+function case2() {
+  const map = Object.fromEntries(SORT_ORDER.map((v, i) => [v, i]))
+
+  const sortedArr = arr.sort((a, b) => map[a] - map[b])
+
+  console.log('sortedArr', sortedArr)
+}
+
+case1()
+case2()
